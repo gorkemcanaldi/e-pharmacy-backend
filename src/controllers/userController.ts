d9ea3917b704e010bcbf6c6817165ff2a316f28f
gorkem can aldi <gorkem.aldi2003@gmail.com>
2026-04-03 19:35:03 +0300

    deploy: production build with cookie fix

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -27,6 +27,7 @@ const registerController = async (req: Request, res: Response) => {
 const loginController = async (req: Request, res: Response) => {
   const userData: LoginInput = loginSchema.parse(req.body);
   const { accessToken, refreshToken } = await loginUser(userData);
+  console.log("refreshToken backendde:", refreshToken);
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     sameSite: "none",
@@ -42,6 +43,7 @@ const loginController = async (req: Request, res: Response) => {
 
 const refreshController = async (req: Request, res: Response) => {
   const userCookie = req.cookies.refreshToken;
+  console.log("req.cookies.refreshToken:", userCookie);
   const { accessToken } = await refreshUser(userCookie);
   res.status(200).send({
     message: "token refreshed",
